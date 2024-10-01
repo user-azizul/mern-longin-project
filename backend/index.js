37bbee399d69125a7fd275553cc2b13cac56b42e
user-azizul <azizul.contact.me@gmail.com>
2024-10-01 09:24:18 +0300

    modified

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,23 @@ const productsRouter = require("./Routes/ProductsRouter.js");
 require("dotenv").config();
 const app = express();
 
-app.get("/ping", (req, res) => {
-  res.send("PONG");
+app.get('/', (req, res) => {
+  try {
+    res.send('Hello from Vercel!');
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
+app.get('/api/hello', (req, res) => {
+  try {
+    res.json({ message: 'Hello from API!' });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 // Apply CORS middleware with options
 app.use(cors({ origin: "https://mern-ui-one.vercel.app" ,credentials:true}));
 app.use(bodyParser.json());
